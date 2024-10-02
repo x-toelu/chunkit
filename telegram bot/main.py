7a5b702c1b850e54cf091d591ed8b2dea91d6801
Toelu <joeltolu9@gmail.com>
2024-10-02 16:04:46 +0100

    chore

--- telegram bot/main.py
+++ telegram bot/main.py
@@ -1,9 +1,8 @@
 from environs import Env
-from telegram import Update
 from telegram.ext import Application, CommandHandler, ConversationHandler, MessageHandler, filters
 
-from config import CONFIRM_CHUNKS, GET_NUM_CHUNKS
 from commands import cancel_command, help_command, set_commands, start_command
+from config import CONFIRM_CHUNKS, GET_NUM_CHUNKS
 from handlers import confirm_chunks, get_num_chunks, handle_file
 
 env = Env()
@@ -18,7 +17,10 @@ def main() -> None:
     application.job_queue.run_once(set_commands, when=1)
 
     conv_handler = ConversationHandler(
-        entry_points=[MessageHandler(filters.Document.ALL, handle_file)],
+        entry_points=[MessageHandler(
+            filters.Document.ALL | filters.PHOTO | filters.VIDEO,
+            handle_file
+        )],
         states={
             GET_NUM_CHUNKS: [MessageHandler(filters.TEXT & ~filters.COMMAND, get_num_chunks)],
             CONFIRM_CHUNKS: [MessageHandler(filters.TEXT & ~filters.COMMAND, confirm_chunks)],
@@ -30,13 +32,12 @@ def main() -> None:
     application.add_handler(CommandHandler("help", help_command))
     application.add_handler(conv_handler)
 
-        # Start the webhook
     application.run_webhook(
-        listen="0.0.0.0",  # Listen on all available network interfaces
-        # port=84443,
-        url_path=env.str("BOT_TOKEN"),  # Use the bot token as the URL path
+        listen="0.0.0.0",
+        url_path=env.str("BOT_TOKEN"),
         webhook_url=f"https://chunkit-voe5.onrender.com/{env.str('BOT_TOKEN')}"
     )
+
 
 if __name__ == "__main__":
     main()
